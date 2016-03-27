22bd573a7042b8cbca3176ac344e9cd0a3c684f6
Andrew Caya <andrewscaya@yahoo.ca>
2016-03-27 17:33:28 -0400

    Basic Application Setup.

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
+use Zend\View\Model\FeedModel;
+
+class IndexController extends AbstractActionController
+{
+	public function onBootstrap($e)
+	{
+		/*$e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
+			$controller = $e->getTarget();
+			$controller->redirect()->toRoute('app-foo');
+		}, 100);*/
+	}
+	
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function rssAction()
+    {
+		$output = [
+			'title'       => 'Hi Martin!',
+			'link'        => 'http://localhost:81/',
+			'description' => 'http://localhost:81 Martin and Andrew learning ZF2!',
+			'items'        => [
+				'title'       => 'Item 1',
+				'link'        => 'http://localhost:81/application/index/index',
+				'description' => 'Martin and Andrew learning ZF2!',
+			],
+		];
+		
+		return new FeedModel($output);
+		//return new JsonModel($output);
+        //return new ViewModel();
+    }
+}
